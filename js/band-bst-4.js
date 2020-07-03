42e87776fe2be3b9af40f055d5c26d07bdf70926
SoundSpinning <SoundSpinning@hotmail.com>
2020-07-03 17:55:30 +0100

    lazy load iframes

--- js/band-bst-4.js
+++ js/band-bst-4.js
@@ -1,42 +1,18 @@
-// 
-// lazyloading of images
-// https://www.smashingmagazine.com/2019/05/hybrid-lazy-loading-progressive-migration-native/
-
-// (function() {
-//   if ("loading" in HTMLImageElement.prototype) {
-//     var lazyEls = document.querySelectorAll("[loading=lazy]");
-//     lazyEls.forEach(function(lazyEl) {
-//       lazyEl.setAttribute(
-//         "src",
-//         lazyEl.getAttribute("data-src")
-//       );
-//     });
-//   } else {
-//     // Dynamically include a lazy loading library of your choice
-//     // Here including vanilla-lazyload
-//     var script = document.createElement("script");
-//     script.async = true;
-//     script.src =
-//       "https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.0.0/dist/lazyload.min.js";
-//     window.lazyLoadOptions = {
-//       elements_selector: "[loading=lazy]"
-//       //eventually more options here
-//     };
-//     document.body.appendChild(script);
-//   }
-// })();
-
-
 // Initialize Tooltip
 $(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="tooltip"]').tooltip({
+    trigger: 'hover'
+  });
+  $('[data-toggle="tooltip"]').on('click', function () {
+    $(this).tooltip('hide')
+  });
 })
 
 // Get the height of the fixed nav bar
 var elnav = $("nav");
 
 // var elnav = document.querySelector('.navbar');
-var headerHeight = 12.0;
+var headerHeight = 10.0;
 if (elnav !== null) {
     headerHeight = headerHeight + elnav.height();
 } 
@@ -47,7 +23,7 @@ $(document).ready(function(){
     e.preventDefault();
     var thistarget = this.hash;
     $thistarget = $(thistarget);
-    $('html, body').stop().animate({
+    $('html').stop().animate({
         'scrollTop': $thistarget.offset().top - headerHeight
         }, 1000, 'swing' 
     );
