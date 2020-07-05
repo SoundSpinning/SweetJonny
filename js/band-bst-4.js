bf9d75ef63e19e0df10f4d17796f2a1358653b9f
SoundSpinning <SoundSpinning@hotmail.com>
2020-07-05 18:44:51 +0100

    removed jquery full

--- js/band-bst-4.js
+++ js/band-bst-4.js
@@ -9,25 +9,25 @@ $(document).ready(function(){
 })
 
 // Get the height of the fixed nav bar
-var elnav = $("nav");
+// var elnav = $("nav");
 
 // var elnav = document.querySelector('.navbar');
-var headerHeight = 10.0;
-if (elnav !== null) {
-    headerHeight = headerHeight + elnav.height();
-} 
+// var headerHeight = 10.0;
+// if (elnav !== null) {
+//     headerHeight = headerHeight + elnav.height();
+// } 
 // Smooth scroll snippet
 // =====================
-$(document).ready(function(){
-    $('a[href*="#"]:not([href*="#ns-"]').on('click',function (e) {
-    e.preventDefault();
-    var thistarget = this.hash;
-    $thistarget = $(thistarget);
-    $('html').stop().animate({
-        'scrollTop': $thistarget.offset().top - headerHeight
-        }, 1000, 'swing' 
-    );
-});
-return false;
-});
+// $(document).ready(function(){
+//     $('a[href*="#"]:not([href*="#ns-"]').on('click',function (e) {
+//     e.preventDefault();
+//     var thistarget = this.hash;
+//     $thistarget = $(thistarget);
+//     $('html').stop().animate({
+//         'scrollTop': $thistarget.offset().top - headerHeight
+//         }, 1000, 'swing' 
+//     );
+// });
+// return false;
+// });
 
