45439e83e8635d1f0ce3b1b1f74a92a85459cb2a
SoundSpinning <SoundSpinning@hotmail.com>
2020-07-08 16:28:39 +0100

    add pics, favicons

--- js/band-bst-4.js
+++ js/band-bst-4.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
 
 // Register service worker to control making site work offline
 
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('./serviceWorker.js')
-           .then(function() { console.log('Service Worker Registered'); });
-}
+// if('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//            .register('./serviceWorker.js')
+//            .then(function() { console.log('Service Worker Registered'); });
+// }
