29457c450e05bda53a33c75424f2d08516336c4e
SoundSpinning <SoundSpinning@hotmail.com>
2020-07-09 11:14:03 +0100

    serviceWorker

--- js/band-bst-4.js
+++ js/band-bst-4.js
@@ -10,9 +10,21 @@ $(document).ready(function(){
 
 
 // Register service worker to control making site work offline
+navigator.serviceWorker.register('../serviceWorker.js');
 
 // if('serviceWorker' in navigator) {
 //   navigator.serviceWorker
-//            .register('./serviceWorker.js')
+//            .register()
 //            .then(function() { console.log('Service Worker Registered'); });
+// }
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('./sw.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
 // }
