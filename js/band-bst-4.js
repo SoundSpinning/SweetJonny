c2dd91183a0d0e312005c91d904f82e437c1393f
SoundSpinning <SoundSpinning@hotmail.com>
2022-11-27 17:57:19 +0000

    add top banner

--- js/band-bst-4.js
+++ js/band-bst-4.js
@@ -3,9 +3,10 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 // Check that service workers are supported
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('../sw.js');
-  });
-}
+// TODO: check WPA works, not a prio for now
+// if ('serviceWorker' in navigator) {
+//   // Use the window load event to keep the page load performant
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('../sw.js');
+//   });
+// }
